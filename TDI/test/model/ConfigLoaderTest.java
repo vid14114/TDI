75405354aeaa8f876b33a125c9dc47459ac0332e
Anil <anil@outlook.at>
2013-11-17 18:59:45 +0100

    Implemented configloader basic tests

--- TDI/test/model/ConfigLoaderTest.java
+++ TDI/test/model/ConfigLoaderTest.java
@@ -2,13 +2,34 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
 import org.junit.Test;
 
 public class ConfigLoaderTest {
-
-	@Test
+	ConfigLoader c = new ConfigLoader();
 	public void test() {
-		fail("Not yet implemented");
+		loadIconsTest();
+		loadWallpaperTest();
+		loadScreensizeTest();
 	}
+	
+	@Test
+	public void loadIconsTest(){
+		assertTrue(ConfigLoader.loadIcons() instanceof String);
+	}
+	
+	@Test
+	public void loadWallpaperTest(){
+		assertTrue(ConfigLoader.loadWallpaper() instanceof BufferedImage);
+	}
+	
+	@Test
+	public void loadScreensizeTest(){
+		assertTrue(c.loadScreensize() instanceof Point);
+	}
+	
+	
 
 }
