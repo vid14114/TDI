13933834351d9d98d1fbffb77d4cb15fa231112f
Abideen Bamgbala <bam14053@spengergasse.at>
2013-12-01 10:21:18 +0100

    Started ProgramHandler, Added TODOs

--- TDI/src/controller/ProgramHandler.java
+++ TDI/src/controller/ProgramHandler.java
@@ -1,60 +1,108 @@
 package controller;
 
-public class ProgramHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ProgramHandler {//TODO how will this class be called, by whom
 
 	/**
-	 * Array of ids of the programs, the first one is the slected window
+	 * An arraylist of wmctrlIDs of the programs currently running
 	 */
-	private static String[] runningPrograms;
+	private static ArrayList<String> runningPrograms = new ArrayList<>();
 
 	/**
-	 * Opens the given program and saves the id in the list.
-	 * @param programPath
+	 * Opens the given program and saves the wmctrlID in the list.
+	 * @param exec The execution path of the program
 	 */
-	public static void openProgram(String programPath) {
-		throw new UnsupportedOperationException();
+	public static void openProgram(String[] exec) {
+		try{
+			Executor.executeProgram(exec);	
+			String wmctrlID = null;			
+			while(wmctrlID == null){
+				BufferedReader reader = Executor.getRunningTasks();
+				String line;
+				while((line = reader.readLine()) != null){
+					if(exec[1].equals("trash:///"))//The exec path of trash (trash:///) is either shown as rubbish bin or trash by wmctrl
+						if((line.contains("Rubbish Bin") || line.contains("Trash")) && !runningPrograms.contains(line.split(" ")[0].replaceFirst("0x", "")))
+							wmctrlID = line.split(" ")[0].replaceFirst("0x", "");
+					else if(line.contains(exec[1]) && !runningPrograms.contains(line.split(" ")[0]))
+						wmctrlID = line.split(" ")[0];
+				}
+			}
+			runningPrograms.add(0,wmctrlID);
+		}catch(IOException e){
+		}
 	}
 
 	/**
 	 * Closes the program and removes it from the programs ArrayList
 	 */
 	public static void closeProgram() {
-		throw new UnsupportedOperationException();
+		try {
+			Runtime.getRuntime().exec(new String[]{"wmctrl", "-i", "-c" , Executor.getFocusedWindow()});
+			runningPrograms.remove(Executor.getFocusedWindow());
+		} catch (IOException e) {
+		}
 	}
 
 	/**
 	 * Closes all programs
 	 */
 	public static void closeAllPrograms() {
-		throw new UnsupportedOperationException();
+		try {
+			for(String process: runningPrograms)
+				Runtime.getRuntime().exec(new String[]{"wmctrl", "-i", "-c" ,process});
+			runningPrograms.clear();
+		} catch (IOException e) {
+
+		}
 	}
 
 	/**
-	 * Toggles between minimize and maximize (gets the program from the programs array)
-	 */
+	 * Toggles between minimised and restored (gets the program from the programs array)
+	 * 
+	 *///TODO rewrite
 	public static void toggleMinimization() {
-		throw new UnsupportedOperationException();
+        try {//TDOD wmctrl -i -a  0x01400005 <-- use for restoring
+    		if(runningPrograms.size() == 1)
+    			minimizeAllPrograms();
+    		else
+                Runtime.getRuntime().exec(new String[]{"wmctrl", "-i", "-r", Executor.getFocusedWindow(), "-b", "add,below"});
+        } catch (IOException e) {
+                System.err.println("An error happened while trying to minimize a program");
+        }
 	}
 
 	/**
 	 * Minimizes all programs
 	 */
 	public static void minimizeAllPrograms() {
-		throw new UnsupportedOperationException();
+		try {	
+			Runtime.getRuntime().exec(new String[]{"wmctrl", "-k", "on"});
+		} catch (IOException e) {
+		}
 	}
 
 	/**
 	 * Restores all programs
 	 */
 	public static void restoreAllPrograms() {
-		throw new UnsupportedOperationException();
+		try {	
+			Runtime.getRuntime().exec(new String[]{"wmctrl", "-k", "off"});
+		} catch (IOException e) {
+		}
 	}
 
 	/**
 	 * Toggles between maximize and non-maximized
 	 */
 	public static void toggleMaximization() {
-		throw new UnsupportedOperationException();
+		try {
+			Runtime.getRuntime().exec(new String[]{"wmctrl", "-i", "-r", Executor.getFocusedWindow(), "-b", "toggle","maximized_vert","maximized_horz"});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
