aeb75963651c1641262506ca5d4e6cf229ae3e3f
Viktor Vidovic <vidovicviktor@gmail.com>
2013-12-09 09:15:01 +0100

    implement sending of byteMessages

--- TDI/src/model/Server.java
+++ TDI/src/model/Server.java
@@ -1,185 +1,211 @@
 package model;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
  * Implements the runnable interface
  * 
- * RUN METHOD CALLS ADD METHOD FROM BIGLOGIC;IS MASTER, IS BIG.
- * IS SLIGHTLY SMALLER, IS NOT SO BIG.
+ * RUN METHOD CALLS ADD METHOD FROM BIGLOGIC;IS MASTER, IS BIG. IS SLIGHTLY
+ * SMALLER, IS NOT SO BIG.
  */
-public class Server implements Runnable{
+public class Server {
 
 	/**
 	 * 
 	 * ip
 	 */
 	protected static String ip;
-	private Socket client; //we are a client, smartphone is the server
+	private Socket client; // we are a client, smartphone is the server
 	private static ObjectOutputStream send;
-	
-	public Server(){
-		try {
-            //Created a new socket which is bound to the port 2345
-            client = new Socket(ip, 2345);
-            Thread t=new Thread(this);
-            t.start();
-    } catch (IOException e) {
-    	System.out.print("Error, could not connect to port 2345");
-    }
-	}
+	private static ObjectInputStream read;
 
-	@Override
-	public void run() {
-		while(true){
-            try{
-                    client.setKeepAlive(true);
-                    ObjectInputStream read = new ObjectInputStream(client.getInputStream());
-                    send = new ObjectOutputStream(client.getOutputStream());                                
-                    while(client.isConnected()){ //Here the whole message handling is done
-                            String[] message = ((String)read.readObject()).split(";"); //For some reasons, only when the objectoutputstream sends messages, can they be read by the server
-                            switch(message[0].toLowerCase()){
-                            case "LULZ": System.out.println("Lulz was sent"); break;
-                            default: send.writeObject("Unknown command"); break;
-                            }                                
-                    }
-            }
-            catch(IOException e){
-                    e.printStackTrace();
-                    System.out.println(e.getCause().getMessage());
-            } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                    System.out.println(e.getCause().getMessage());
-            }
+	public Server() {
+		try {
+			// Created a new socket which is bound to the port 2345
+			client = new Socket(ip, 2345);
+			client.setKeepAlive(true);
+			send = new ObjectOutputStream(client.getOutputStream());
+			read = new ObjectInputStream(client.getInputStream());
+		} catch (IOException e) {
+			System.out.print("Error, could not connect to port 2345");
 		}
 	}
 
-	/**
-	 * Sends the specified command
-	 */
-	public static void sendCommand(ACTOData cmd) {
+	public static void fullPose() {
 		try {
-            send.writeObject(cmd);
-	    } catch (IOException e) {
-	            e.printStackTrace();
-	    }
+			send.writeByte(ACTOConst.WI_FULL_POSE);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	// ask pose update of all TUIOs
-	// format: CMD
-	// 51
-	public static void fullPose()
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_FULL_POSE);
-		sendCommand(a);
+	public static void fullTrans() {
+		try {
+			send.writeByte(ACTOConst.WI_FULL_TRANS);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	// ask only translation update of all TUIOs
-	// format: CMD
-	// 53
-	public static void fullTrans()
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_FULL_TRANS);
-		sendCommand(a);
+
+	public static void fullRot() {
+		try {
+			send.writeByte(ACTOConst.WI_FULL_ROT);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	// ask only rotation update of all TUIOs	
-	// format: CMD
-	// 54
-	public static void fullRot()
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_FULL_ROT);
-		sendCommand(a);
+
+	public static void getPose(byte id) {
+		try {
+			send.writeByte(ACTOConst.WI_GET_POSE);
+			send.writeByte(id);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	// ask pose update of specific TUIO
-	// format: CMD, ID
-	// 61
-	public static void getPose(byte id)
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_GET_POSE, id);
-		sendCommand(a);
+
+	public static void getTrans(byte id) {
+		try {
+			send.writeByte(ACTOConst.WI_GET_TRANS);
+			send.writeByte(id);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	// set/send state of specific TUIO
-	// format: CMD, ID
-	// 63
-	public static void getTrans(byte id)
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_GET_TRANS, id);
-		sendCommand(a);
+
+	public static void getRot(byte id) {
+		try {
+			send.writeByte(ACTOConst.WI_GET_ROT);
+			send.writeByte(id);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	// ask only rotation update of specific TUIO
-	// format: CMD, ID
-	// 64
-	public static void getRot(byte id)
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_GET_ROT, id);
-		sendCommand(a);
+
+	public static void getPlsize() {
+		try {
+			send.writeByte(ACTOConst.WI_GET_PLSIZE);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
-	// ask resolution of playground	
-	// format: CMD
-	// 65
-	public static void getPlsize()
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_GET_PLSIZE);
-		sendCommand(a);
+
+	public static void setPose(byte id, float[] trans, float[] rot) {
+		try {
+			send.writeByte(ACTOConst.WI_SET_POSE);
+			send.writeByte(id);
+			send.writeFloat(trans[0]);
+			send.writeFloat(trans[1]);
+			send.writeFloat(trans[2]);
+			send.writeFloat(rot[0]);
+			send.writeFloat(rot[1]);
+			send.writeFloat(rot[2]);
+			send.writeFloat(rot[3]);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	// set/send state of specific TUIO
-	// format: CMD, ID, float[3], float[4], ???
-	// 80
-	public static void setState(byte id, float[] trans, float[] rot)
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_SET_STATE, id, trans, rot);
-		sendCommand(a);
+
+	public static void setExt(byte id) { // Not yet implemented in
+											// WifiTransmitter
+		try {
+			send.writeByte(ACTOConst.WI_SET_EXT);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	// set/send pose of specific TUIO	
-	// format: CMD, ID, float[3], float[4]
-	// 81
-	public static void setPose(byte id, float[] trans, float[] rot)
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_SET_POSE, id, trans, rot);
-		sendCommand(a);
-	}
-	
-	// set/send ext state of specific TUIO	
-	// format: CMD, ID, ???
-	// 82
-	public static void setExt(byte id)
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_SET_EXT, id);
-		sendCommand(a);
-	}
-	
-	// set/send only translation update of specific TUIO	
+
+	// set/send only translation update of specific TUIO
 	// format: CMD, ID, float[3]
 	// 83
-	public static void setTrans(byte id, float[] rot)
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_SET_TRANS, id, rot);
-		sendCommand(a);
-	}
-	
-	// set/send only rotation update of specific TUIO	
-	// format: CMD, ID, float[4]
-	// 84
-	public static void setRot(byte id, float[] trans)
-	{
-		ACTOData a=new ACTOData(ACTOConst.WI_SET_ROT, id, trans);
-		sendCommand(a);
+	public static void setTrans(byte id, float[] trans) {
+		try {
+			send.writeByte(ACTOConst.WI_SET_TRANS);
+			send.writeByte(id);
+			send.writeFloat(trans[0]);
+			send.writeFloat(trans[1]);
+			send.writeFloat(trans[2]);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	
+	public static void setRot(byte id, float[] rot) {
+		try {
+			send.writeByte(ACTOConst.WI_SET_ROT);
+			send.writeByte(id);
+			send.writeFloat(rot[0]);
+			send.writeFloat(rot[1]);
+			send.writeFloat(rot[2]);
+			send.writeFloat(rot[3]);
+			byte ack = 0;
+			while (ack != ACTOConst.WI_ACK) {
+				ack = read.readByte();
+				// TODO handle input
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
