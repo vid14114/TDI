3960b45a2bb3a12890368529439640d394b80e97
anil <anil@outlook.at>
2014-02-17 09:22:33 +0100

    Modified the wallpaper

--- TDI/src/view/Wallpaper.java
+++ TDI/src/view/Wallpaper.java
@@ -1,6 +1,9 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 public class Wallpaper {
@@ -28,8 +31,39 @@ public class Wallpaper {
 	 * @param colorAreas
 	 *            The TDIs with their icons, and we will generate the markedArea
 	 */
-	public static void markArea(TDI[] colorAreas) {
-		throw new UnsupportedOperationException();
+	public void markArea(TDI[] colorAreas) {
+		int[][] d = new int[10][10];
+		int tId = 1;
+		
+		//Fill array with ids
+		for(TDI t : colorAreas){
+			for(Icon i : t.getIcons()){
+				d[i.getPosition().x][i.getPosition().y]=tId;
+			}
+			tId++;
+		}
+		
+		int imageWidth = background.getWidth();
+		int imageHeight = background.getHeight();
+		
+		int column=0;
+		for(int[] i : d){
+			int row=0;
+			for(int x : i){
+				Graphics2D g2 = background.createGraphics();
+				//Only draw if the is is not 0 - 0 = no icon
+				if(x==0){
+				}
+				else{
+					g2.setColor(Color.blue);
+					//First 2 values define the position of the icon, the other 2 define the size
+					g2.fill(new Rectangle2D.Float(blockSize*row,blockSize*column,blockSize,blockSize));
+					g2.drawImage(background, 0, 0, null);
+				}
+				row++;
+			}
+			column++;
+		}
 	}
 	/**
 	 * @param the size of icon block
