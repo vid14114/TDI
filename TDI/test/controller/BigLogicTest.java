cd5dfc77ffcc44197d655472c477207b37e297e3
Maria Hasler <has140303@spengergasse.at>
2014-03-17 06:20:39 +0100

    modified my BigLogik Tests

--- TDI/test/controller/BigLogicTest.java
+++ TDI/test/controller/BigLogicTest.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 import model.ConfigLoader;
@@ -30,11 +31,11 @@ public class BigLogicTest {
 	@Test
 	public void testLinksDrehenD()
 	{
-		float[] rot1 = {1,1,1};
-		float[] rot2 = {1,2,1}; //TODO: find right rot 
+		float[] rot1 = {100,100,100};
+		float[] rot2 = {200,100,100}; //TODO: find right rot 
 		byte id= 49;
-		TDI t1 = new TDI(id, 1, 1, 0, rot1);	
-		TDI t2 = new TDI(id, 1, 1, 0, rot2);
+		TDI t1 = new TDI(id, 100, 100, 50, rot1);	
+		TDI t2 = new TDI(id, 100, 100, 50, rot2);
 		ProgramHandler.closeAllPrograms();
 		commands.add(t2);
 		tdis.add(t1);
@@ -44,8 +45,14 @@ public class BigLogicTest {
 		bl.setTdis(tdis);
 		bl.splitIcons();
 		bl.run();
-		Assert.assertEquals(t1.getIcons().get(0).getName(),"gimp");
-		Assert.assertEquals(tdis.get(0).getRotation(),t2.getRotation());
+		for(Icon i:t1.getIcons())
+		{
+			System.out.println(i.getName());
+		}
+		Assert.assertEquals(t1.getIcons().get(0).getName(),"jre1.7.0_09");
+		System.out.println(tdis.get(0).getRotation()[0]);
+		Assert.assertTrue(Arrays.equals(tdis.get(0).getRotation(),t2.getRotation()));
+		
 	}
 	/**
 	 * Test method for
@@ -54,28 +61,23 @@ public class BigLogicTest {
 	@Test
 	public void testRechtsDrehenD()
 	{
-		float[] rot1 = {1,1,1};
-		float[] rot2 = {1,-1,1}; //TODO: find right rot 
-		byte id= 0;
-		TDI t1 = bl.getTdis().get(0);	
-		TDI t2 = bl.getTdis().get(1);	
-		t1.setRotation(rot1);
-		t2.setRotation(rot2);
-		Point p1 = new Point (5,3);
-		Icon i1 = new Icon("mozilla",p1);
-		Point p2 = new Point (6,1);
-		Icon i2 = new Icon("gimp",p2);
+		float[] rot1 = {200,100,100};
+		float[] rot2 = {100,100,100}; //TODO: find right rot 
+		byte id= 49;
+		TDI t1 = new TDI(id, 100, 100, 50, rot1);	
+		TDI t2 = new TDI(id, 100, 100, 50, rot2);
 		ProgramHandler.closeAllPrograms();
-		tdis.add(t1);
 		commands.add(t2);
+		tdis.add(t1);
 		
-	/*	bl.icons=icons;
-		bl.tdis=tdis;
-		bl.commands=commands;
-		*/
+		icons = cl.loadIcons();
+		bl.setCommands(commands);
+		bl.setTdis(tdis);
+		bl.splitIcons();
 		bl.run();
-		Assert.assertEquals(t1.getIcons().get(0).getName(),"mozilla");
-		Assert.assertEquals(tdis.get(0).getRotation(),t2.getRotation());
+
+		Assert.assertEquals(t1.getIcons().get(0).getName(),"File System");
+		Assert.assertTrue(Arrays.equals(tdis.get(0).getRotation(),t2.getRotation()));
 	}
 	/**
 	 * Test method for
@@ -84,29 +86,25 @@ public class BigLogicTest {
 	@Test
 	public void testLinksNeigenW()
 	{
-		float[] rot1 = {1,1,1};
-		float[] rot2 = {-1,1,1}; //TODO: find right rot 
-		byte id= 0;
-		TDI t1 = new TDI(id, 1, 1, 0, rot1);	
-		TDI t2 = new TDI(id, 1, 1, 0, rot2);	
-		Point p1 = new Point (5,3);
-		Icon i1 = new Icon("mozilla",p1);
-		
-		ProgramHandler.closeAllPrograms();
-		ProgramHandler.openProgram(i1);
+		float[] rot1 = {100,100,100};
+		float[] rot2 = {100,200,100}; //TODO: find right rot 
+		byte id= 49;
+		TDI t1 = new TDI(id, 100, 100, 50, rot1);	
 		t1.setState(TDIState.window);
-		tdis.add(t1);
+		TDI t2 = new TDI(id, 100, 100, 50, rot2);
+		ProgramHandler.closeAllPrograms();
 		commands.add(t2);
-		icons.add(i1);
+		tdis.add(t1);
 		
-	/*	bl.tdis=tdis;
-		bl.commands=commands;
-		bl.icons=icons;
-		*/
+		icons = cl.loadIcons();
+		bl.setCommands(commands);
+		bl.setTdis(tdis);
+		bl.splitIcons();
 		bl.run();
-		Assert.assertEquals(ProgramHandler.getRunningPrograms(),0);
-		Assert.assertEquals(tdis.get(0).getRotation(),t2.getRotation());
+
+		Assert.assertTrue(Arrays.equals(tdis.get(0).getRotation(),t2.getRotation()));
 		Assert.assertEquals(tdis.get(0).getState(),TDIState.desktop);
+		Assert.assertEquals(ProgramHandler.getRunningPrograms(),1);
 	}
 	/**
 	 * Test method for
@@ -115,32 +113,29 @@ public class BigLogicTest {
 	@Test
 	public void testRechtsNeigenNichtGelocktD()
 	{
-		float[] rot1 = {1,1,1};
-		float[] rot2 = {2,1,1}; //TODO: find right rot 
-		byte id= 0;
-		TDI t1 = new TDI(id, 1, 1, 0, rot1);	
-		TDI t2 = new TDI(id, 1, 1, 0, rot2);	
-		Point p1 = new Point (5,3);
-		Icon i1 = new Icon("mozilla",p1);
-		
+		float[] rot1 = {100,200,100};
+		float[] rot2 = {100,100,100}; //TODO: find right rot 
+		byte id= 49;
+		TDI t1 = new TDI(id, 100, 100, 50, rot1);	
+		t1.setState(TDIState.desktop);
+		TDI t2 = new TDI(id, 100, 100, 50, rot2);
 		ProgramHandler.closeAllPrograms();
+		commands.add(t2);
+		tdis.add(t1);
 		if(t1.getLocked())
 		{
 			t1.toggleLock();
 		}
-		tdis.add(t1);
-		icons.add(i1);
-		commands.add(t2);
 		
-	/*	bl.tdis=tdis;
-		bl.commands=commands;
-		bl.icons=icons;*/
-		
+		icons = cl.loadIcons();
+		bl.setCommands(commands);
+		bl.setTdis(tdis);
+		bl.splitIcons();
 		bl.run();
 		Assert.assertTrue(tdis.get(0).getLocked());
 		//TODO check if green led is on
 		Assert.assertEquals(tdis.get(0).getState(),TDIState.desktop);
-		Assert.assertEquals(tdis.get(0).getRotation(),t2.getRotation());
+		Assert.assertTrue(Arrays.equals(tdis.get(0).getRotation(),t2.getRotation()));
 	}
 	/**
 	 * Test method for
@@ -149,32 +144,29 @@ public class BigLogicTest {
 	@Test
 	public void testRechtsNeigenGelocktD()
 	{
-		float[] rot1 = {1,1,1};
-		float[] rot2 = {2,1,1}; //TODO: find right rot 
-		byte id= 0;
-		TDI t1 = new TDI(id, 1, 1, 0, rot1);	
-		TDI t2 = new TDI(id, 1, 1, 0, rot2);	
-		Point p1 = new Point (5,3);
-		Icon i1 = new Icon("mozilla",p1);
-		
+		float[] rot1 = {100,200,100};
+		float[] rot2 = {100,100,100}; //TODO: find right rot 
+		byte id= 49;
+		TDI t1 = new TDI(id, 100, 100, 50, rot1);	
+		t1.setState(TDIState.desktop);
+		TDI t2 = new TDI(id, 100, 100, 50, rot2);
 		ProgramHandler.closeAllPrograms();
+		commands.add(t2);
+		tdis.add(t1);
 		if(t1.getLocked()==false)
 		{
 			t1.toggleLock();
 		}
-		icons.add(i1);
-		tdis.add(t1);
-		commands.add(t2);
 		
-/*		bl.tdis=tdis;
-		bl.commands=commands;
-		bl.icons=icons;*/
-		
+		icons = cl.loadIcons();
+		bl.setCommands(commands);
+		bl.setTdis(tdis);
+		bl.splitIcons();
 		bl.run();
-		Assert.assertTrue(!tdis.get(0).getLocked());
-		//TODO check if green led is not on
+		Assert.assertFalse(tdis.get(0).getLocked());
+		//TODO check if green led is on
 		Assert.assertEquals(tdis.get(0).getState(),TDIState.desktop);
-		Assert.assertEquals(tdis.get(0).getRotation(),t2.getRotation());
+		Assert.assertTrue(Arrays.equals(tdis.get(0).getRotation(),t2.getRotation()));
 	}
 	/**
 	 * Test method for
@@ -183,29 +175,24 @@ public class BigLogicTest {
 	@Test
 	public void testObenNeigenNichtMaximiertW()
 	{
-		float[] rot1 = {1,1,1};
-		float[] rot2 = {1,1,2}; //TODO: find right rot 
-		byte id= 0;
-		TDI t1 = new TDI(id, 1, 1, 0, rot1);	
-		TDI t2 = new TDI(id, 1, 1, 0, rot2);	
-		Point p1 = new Point (5,3);
-		Icon i1 = new Icon("mozilla",p1);
-		
-		ProgramHandler.closeAllPrograms();
-		ProgramHandler.openProgram(i1);
+		float[] rot1 = {100,100,200};
+		float[] rot2 = {100,100,100}; //TODO: find right rot 
+		byte id= 49;
+		TDI t1 = new TDI(id, 100, 100, 50, rot1);	
 		t1.setState(TDIState.window);
-		icons.add(i1);
-		tdis.add(t1);
+		TDI t2 = new TDI(id, 100, 100, 50, rot2);
 		commands.add(t2);
+		tdis.add(t1);
 		
-	/*	bl.tdis=tdis;
-		bl.commands=commands;
-		bl.icons=icons;*/
-		
+		icons = cl.loadIcons();
+		bl.setCommands(commands);
+		bl.setTdis(tdis);
+		bl.splitIcons();
 		bl.run();
+		
 		//Assert.assertEquals(ProgramHandler.); TODO get Maximised Programs
 		Assert.assertEquals(ProgramHandler.getNonMinimized(),1);
-		Assert.assertEquals(tdis.get(0).getRotation(),t2.getRotation());
+		Assert.assertTrue(Arrays.equals(tdis.get(0).getRotation(),t2.getRotation()));
 	}
 	/**
 	 * Test method for
